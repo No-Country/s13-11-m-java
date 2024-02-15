f6b7cdae065f7e4c706620402027b79d2b44623a
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-02-15 14:36:35 -0300

    labels styled

--- frontend/src/components/RegisterForm/RegisterForm.tsx
+++ frontend/src/components/RegisterForm/RegisterForm.tsx
@@ -17,43 +17,45 @@ export default function RegisterForm() {
   });
   const onSubmit: SubmitHandler<RegisterFormInputs> = (data) => console.log(data);
 
+  const inputStyle = "border-b-4 border-primary";
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-center">
-      <div className="grid w-[460px] grid-cols-2 gap-4">
+    <form onSubmit={handleSubmit(onSubmit)} className="mx-auto flex flex-col justify-center">
+      <div className=" grid w-[460px] grid-cols-2 gap-4">
         <div>
           <Label htmlFor="email">Mail</Label>
-          <Input type="email" {...register("email")} />
+          <Input type="email" {...register("email")} className={inputStyle} />
           <ErrorMessage error={errors.email} />
         </div>
 
         <div>
           <Label htmlFor="password">Contraseña</Label>
-          <Input type="password" {...register("password")} />
+          <Input type="password" {...register("password")} className={inputStyle} />
           <ErrorMessage error={errors.password} />
         </div>
         <div>
           <Label htmlFor="firstName">Nombre</Label>
-          <Input type="text" {...register("firstName")} />
+          <Input type="text" {...register("firstName")} className={inputStyle} />
           <ErrorMessage error={errors.firstName} />
         </div>
         <div>
           <Label htmlFor="lastName">Apellido</Label>
-          <Input type="text" {...register("lastName")} />
+          <Input type="text" {...register("lastName")} className={inputStyle} />
           <ErrorMessage error={errors.lastName} />
         </div>
         <div>
           <Label htmlFor="address">Direccion</Label>
-          <Input type="text" {...register("address")} />
+          <Input type="text" {...register("address")} className={inputStyle} />
           <ErrorMessage error={errors.address} />
         </div>
         <div>
-          <Label htmlFor="phone">Teléfono</Label>
-          <Input type="tel" {...register("phone")} />
+          <Label htmlFor="phone">Numero de Teléfono</Label>
+          <Input type="tel" {...register("phone")} className={inputStyle} />
           <ErrorMessage error={errors.phone} />
         </div>
       </div>
 
-      <Button type="submit" className="mx-auto mt-3">
+      <Button type="submit" className="mx-auto mt-3 w-full rounded-full">
         Registrarse
       </Button>
     </form>
--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -1,15 +1,22 @@
+import OnBoardingHeader from "@/components/OnBoardingHeader";
 import RegisterForm from "@/components/RegisterForm/RegisterForm";
 
 const Register = () => {
   return (
-    <div className="flex min-h-[900px]">
-      <section className="left-0 flex w-1/3 items-center justify-center bg-[#00304B]">
-        <img src="@/assets/bro.png" />
-      </section>
-      <section className="right-0 flex w-2/3 flex-col items-center justify-center">
-        <header className="mb-3 text-start text-4xl">Crea tu cuenta</header>
-        <RegisterForm />
-      </section>
+    <div className="h-screen w-full">
+      <OnBoardingHeader />
+      <div className="h-[calc(100%-70px)] md:flex">
+        <section className="bg-primary flex-col items-center justify-center md:flex  md:w-1/3">
+          <h1 className="mx-auto max-w-[80%] text-3xl text-white">
+            Controlá tu producción, alcanzá tus metas: Tu socio digital de confianza.
+          </h1>
+          <img src="/src/assets/register.svg" />
+        </section>
+        <section className="right-0 flex w-2/3 flex-col items-center justify-center">
+          <header className="mb-3 text-start text-4xl">Crea tu cuenta</header>
+          <RegisterForm />
+        </section>
+      </div>
     </div>
   );
 };
