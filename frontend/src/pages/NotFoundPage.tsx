017b6ab807bef58e8ed64dd7f78b676c9f30076d
lucenstuff <lucentiniagustin@hotmail.com>
2024-02-15 15:47:38 -0300

    added 404page

---
+++ frontend/src/pages/NotFoundPage.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+
+function NotFoundPage() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold text-gray-800">404</h1>
+        <p className="text-2xl font-light text-gray-600">Página no encontrada</p>
+        <p className="mt-4 text-gray-600">Lo sentimos, la página que buscas no existe o fue movida.</p>
+        <Button className="mt-6" variant="default">
+          {" "}
+          Volver al inicio
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default NotFoundPage;
