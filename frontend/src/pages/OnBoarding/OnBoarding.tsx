149342dc886d2baf4b73a94c9a67eaf4fbe7d3b7
lucenstuff <lucentiniagustin@hotmail.com>
2024-02-15 16:02:17 -0300

    added autoplay to onboarding

--- frontend/src/pages/OnBoarding/OnBoarding.tsx
+++ frontend/src/pages/OnBoarding/OnBoarding.tsx
@@ -15,6 +15,8 @@ const settings = {
   speed: 700,
   slidesToShow: 1,
   slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
 };
 
 const Carousel: React.FC = () => {
@@ -25,10 +27,10 @@ const Carousel: React.FC = () => {
         <Slider {...settings}>
           <div>
             <div className="flex justify-center">
-              <div className="flex justify-center items-start pt-32">
-                <div className="flex flex-col w-1/4">
+              <div className="flex items-start justify-center pt-32">
+                <div className="flex w-1/4 flex-col ">
                   <h2 className="text-4xl">Registrá la producción realizada.</h2>
-                  <p className="text-2xl pt-6 pr-6">
+                  <p className="pr-6 pt-6 text-2xl ">
                     ¿Sabías que podes ingresar las unidades que has producido durante cierto proceso? Esto te dará datos
                     precisos sobre tu producción real y te ayudará a compararla con los objetivos de tu empresa
                   </p>
@@ -38,10 +40,10 @@ const Carousel: React.FC = () => {
             </div>
           </div>
           <div>
-            <div className="flex justify-center items-start pt-32">
-              <div className="flex flex-col w-1/4">
+            <div className="flex items-start justify-center pt-32">
+              <div className="flex w-1/4 flex-col ">
                 <h2 className="text-4xl">Modificá el tiempo de proceso.</h2>
-                <p className="text-2xl pt-6 pr-6">
+                <p className="pr-6 pt-6 text-2xl ">
                   Los trabajadores pueden ajustar el tiempo estimado de un proceso en base a su experiencia o si hay
                   cambios en las condiciones de producción. Esto ayuda a planificar y programar los procesos con mayor
                   precisión.
@@ -51,15 +53,15 @@ const Carousel: React.FC = () => {
             </div>
           </div>
           <div>
-            <div className="flex justify-center items-start pt-32">
-              <div className="flex flex-col w-1/4 justify-start">
+            <div className="flex items-start justify-center pt-32">
+              <div className="flex w-1/4 flex-col ">
                 <h2 className="text-4xl">Añadí notas o aclaraciones.</h2>
-                <p className="text-2xl pt-6 pr-6">
+                <p className="pr-6 pt-6 text-2xl ">
                   Los trabajadores pueden poner notitas o comentarios importantes en un proceso específico referido a
                   cualquier problema que encuentren, ideas para mejorar o cualquier otra observación que sea útil para
                   el equipo de producción.
                 </p>
-                <Button className="mt-10 mr-10 ">Empezar Ahora</Button>
+                <Button className="mr-10 mt-10">Empezar Ahora</Button>
               </div>
               <img src={slideImage3} alt="Man in front of a board" />
             </div>
