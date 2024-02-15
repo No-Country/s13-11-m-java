1e4cacfe2635b8728eb5e7fb71f6725dae430105
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-02-15 15:43:02 -0300

    created - Confirm mail, Error Register & Forgot password pages -

--- frontend/src/routes/mainRoutes.tsx
+++ frontend/src/routes/mainRoutes.tsx
@@ -1,6 +1,10 @@
 import Register from "@/pages/Register/Register";
 import { createBrowserRouter } from "react-router-dom";
 import OnBoarding from "../pages/OnBoarding/OnBoarding";
+import ConfirmEmail from "@/pages/Register/ConfirmEmail";
+import ConfirmEmail2 from "@/pages/Register/ConfirmEmail2";
+import ForgotPassword from "@/pages/Register/ForgotPassword";
+import ErrorRegister from "@/pages/Register/ErrorRegister";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -9,5 +13,21 @@ export const router = createBrowserRouter([
   {
     path: "/register",
     element: <Register />,
+  },
+  {
+    path: "/confirm1",
+    element: <ConfirmEmail />,
+  },
+  {
+    path: "/confirm2",
+    element: <ConfirmEmail2 />,
+  },
+  {
+    path: "/forgotpass",
+    element: <ForgotPassword />,
+  },
+  {
+    path: "/errorregister",
+    element: <ErrorRegister />,
   },
 ]);
