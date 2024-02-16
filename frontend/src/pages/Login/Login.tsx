923136f279c59e3675961f1b4452aff3a9990f39
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2024-02-16 11:03:25 -0300

    add / to icon

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,24 +1,24 @@
 //Para hacer fetch y validacion de mock data
-// import data from '@/data/users.mock.json'
-// {
-//   "username": "admin@admin.com",
-//   "password": "1234"
-// }
+import data from '@/data/users.mock.json'
+
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import slideImage4 from "@/assets/login.svg";
+
+const mockData = data;
+console.log(mockData)
 
 const Login: React.FC = () => {
   return (
     <>
       <div className="flex h-screen w-screen ">
         <div className="container w-1/2 bg-slate-800 ">
-          <img
+          <Link to="/"><img
             className="m-0 size-12 text-white "
             src="https://images.vexels.com/media/users/3/136994/isolated/preview/aa3216bc8664471a5f75363fd74c728a-icono-de-reloj-temporizador.png"
             alt="LOGO"
-          />
+          /></Link>
           <p className="text-md m-10 text-white">Gestioná tu producción de manera inteligente y sencilla</p>
           <img className="pb-5" src={slideImage4} alt="image-login" />
         </div>
