9d8984090570ea6fbac275da2c01ca491df54caf
Santiago Ariel <santiagoarielv98@gmail.com>
2024-02-18 15:16:52 -0300

    ordenamiento de imports en mainRoutes.tsx

--- frontend/src/routes/mainRoutes.tsx
+++ frontend/src/routes/mainRoutes.tsx
@@ -1,14 +1,15 @@
-import Register from "@/pages/Register/Register";
 import { createBrowserRouter } from "react-router-dom";
-import OnBoarding from "../pages/OnBoarding/OnBoarding";
-import NotFoundPage from "@/pages/NotFoundPage";
-import Login from "@/pages/Login/Login";
-import MainLayout from "@/layouts/MainLayout";
+
 import AuthLayout from "@/layouts/AuthLayout";
+import MainLayout from "@/layouts/MainLayout";
+import Login from "@/pages/Login/Login";
+import NotFoundPage from "@/pages/NotFoundPage";
+import OnBoarding from "@/pages/OnBoarding/OnBoarding";
 import ConfirmEmail from "@/pages/Register/ConfirmEmail";
 import ConfirmEmail2 from "@/pages/Register/ConfirmEmail2";
-import ForgotPassword from "@/pages/Register/ForgotPassword";
 import ErrorRegister from "@/pages/Register/ErrorRegister";
+import ForgotPassword from "@/pages/Register/ForgotPassword";
+import Register from "@/pages/Register/Register";
 
 export const router = createBrowserRouter([
   {
