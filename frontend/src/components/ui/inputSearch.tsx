787a4fbd84fa1d07464789d6b2db4e410eb915fb
Santiago Ariel <santiagoarielv98@gmail.com>
2024-02-18 21:31:17 -0300

    agregando nuevo size al boton & inputSearch agregando un poco de separacion

--- frontend/src/components/ui/inputSearch.tsx
+++ frontend/src/components/ui/inputSearch.tsx
@@ -7,11 +7,11 @@ export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> 
 const InputSearch = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
   return (
     <div className="relative">
-      <IoSearch className="absolute left-2 top-3 h-4 w-4" />
+      <IoSearch className="absolute left-4 top-3 h-4 w-4" />
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none  focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 pl-8",
+          "flex h-10 w-full rounded-xl border border-input bg-background px-3 py-2 pl-12 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
