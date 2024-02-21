06f569d7cde19c227604af1b0f903627ca174c81
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2024-02-21 19:45:26 -0300

    add bar chart & Progress & some visual fixes with Darshboard page

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -8,6 +8,7 @@ import { Doughnut } from "react-chartjs-2";
 import { Bar } from "react-chartjs-2";
 import { options, data as barData } from "@/data/Dashboard/bar/bar.chart";
 import ProductsPage from "../Products/ProductsPage";
+import { Progress } from "@/components/ui/progress";
 
 ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement, Title);
 
@@ -15,22 +16,30 @@ const Dashboard = () => {
   return (
     <>
       <Sidebar />
-      <div className="w-full">
-        <div className="w-3/4 flex-wrap justify-center">
+      <div className="m-5 flex w-full flex-wrap">
+        <div className="justify-content w-3/4 flex-wrap">
           <ProductsPage />
         </div>
-        <div className="flex w-1/4">
-          <div className="block w-1/2"></div>
-          <div className="block w-1/2">
+        <div className="flex w-1/4 flex-col justify-between">
+          <div className="mb-4 mt-8">
+            <h2 className="mb-4 mt-4 px-8 text-3xl">
+              <b>Progresos Activos</b>
+            </h2>
+            <label className="m-1 mb-4 mt-4 px-8 text-3xl">04</label>
+            <Progress value={33} />
+          </div>
+          <div className="mb-4 mt-4">
             <Bar options={options} data={barData} />
           </div>
         </div>
-        <div className="w-1/4"></div>
-        <div className="flex w-1/4">
-          <Doughnut data={dataProcessPorcent} />
-          <Doughnut data={dataProcessQ} />
-          <Doughnut data={dataEmployee} />
-          <Doughnut data={dataProcessQ2} />
+        <div className="m-10 w-1/2 flex-wrap">
+          <div className="justify-content flex w-1/2 flex-nowrap">
+            <Doughnut data={dataProcessPorcent} />
+            <Doughnut data={dataProcessQ} />
+            <Doughnut data={dataEmployee} />
+            <Doughnut data={dataProcessQ2} />
+          </div>
+          <div></div>
         </div>
       </div>
     </>
