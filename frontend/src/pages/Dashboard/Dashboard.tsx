fa4ba6ef88c4f739a538c7523e79eb1500e14b94
Santiago Ariel <santiagoarielv98@gmail.com>
2024-02-22 08:24:51 -0300

    retoques

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -14,12 +14,11 @@ import {
   PointElement,
   LineElement,
 } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
-import { Bar } from "react-chartjs-2";
+
+import { Doughnut, Bar, Line } from "react-chartjs-2";
 import { options, data as barData } from "@/data/Dashboard/bar/bar.chart";
 import { optionsComparative, dataComparative } from "@/data/Dashboard/comparative/comparative.chart";
 import { Progress } from "@/components/ui/progress";
-import { Line } from "react-chartjs-2";
 import { DataTable } from "@/components/ui/data-table";
 import { columns } from "../Products/columns";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
@@ -40,8 +39,8 @@ ChartJS.register(
 
 const Dashboard = () => {
   return (
-    <div className="container grid max-w-full grid-flow-row grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
-      <div className="col-span-full md:col-span-3 md:row-span-2">
+    <div className="container grid max-w-full grid-flow-row grid-cols-1 gap-8 py-8 sm:grid-cols-2 md:grid-cols-3 md:pl-20 lg:grid-cols-4">
+      <div className="col-span-full h-full max-h-[26rem] rounded-2xl bg-background p-4 shadow-2xl md:col-span-3 md:row-span-2">
         <ScrollArea className="h-full whitespace-nowrap rounded-md border">
           <div className="flex">
             <DataTable columns={columns} data={products} />
@@ -49,7 +48,7 @@ const Dashboard = () => {
           <ScrollBar orientation="horizontal" />
         </ScrollArea>
       </div>
-      <div className="h-max rounded-2xl bg-background p-4 shadow-2xl">
+      <div className="rounded-2xl bg-background p-4 shadow-2xl">
         <div>
           <h2 className="mb-4 mt-4 px-8 text-3xl">
             <b>Progresos Activos</b>
@@ -58,7 +57,7 @@ const Dashboard = () => {
           <Progress value={33} />
         </div>
       </div>
-      <div className="h-max rounded-2xl bg-background p-4 shadow-2xl">
+      <div className="rounded-2xl bg-background p-4 shadow-2xl">
         <div>
           <Bar options={options} data={barData} />
         </div>
@@ -70,7 +69,7 @@ const Dashboard = () => {
         <Doughnut data={dataProcessQ} />
       </div>
       <div className="col-span-full rounded-2xl bg-background p-4 shadow-2xl max-lg:order-5 lg:col-span-2 lg:row-span-2">
-        <Line options={optionsComparative} data={dataComparative} />
+        <Line options={optionsComparative} data={dataComparative} style={{ width: "100%" }} />
       </div>
       <div className="rounded-2xl bg-background p-4 shadow-2xl">
         <Doughnut data={dataEmployee} />
