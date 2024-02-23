8327fa4b16c0ffc2b02f0b34545b4f32e78c10d3
Santiago Ariel <santiagoarielv98@gmail.com>
2024-02-23 12:24:44 -0300

    agregando mocks a los endpoints

--- frontend/src/app/services/api/index.ts
+++ frontend/src/app/services/api/index.ts
@@ -1,7 +1,24 @@
 import { apiUrl, authCredentials, registerCredentials } from "@/constants/api";
 import { simulateLoading } from "@/utils/fakeUtils";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { LoginRequest, UserResponse, AllProductsResponse } from "./types";
+import {
+  LoginRequest,
+  UserResponse,
+  AllProductsResponse,
+  GetProductByNameRequest,
+  GetProductByNameResponse,
+  GetProductByIdRequest,
+  GetProductByIdResponse,
+  GetProductByUnicoIdRequest,
+  GetProductByUnicoIdResponse,
+  UpdateProductRequest,
+  UpdateProductResponse,
+  CreateProductRequest,
+  CreateProductResponse,
+  DeleteProductRequest,
+  DeleteProductResponse,
+} from "./types";
+import { RootState } from "@/app/store";
 
 export const api = createApi({
   reducerPath: "api",
@@ -65,7 +82,94 @@ export const api = createApi({
         return { data: json.default as AllProductsResponse };
       },
     }),
+    getProductByName: builder.query<GetProductByNameResponse, GetProductByNameRequest>({
+      // query: (name) => `products/product-name/${name}`,
+      queryFn: async (name, api) => {
+        await simulateLoading();
+
+        const state = api.getState() as RootState;
+        const { data } = (state.api.queries.getAllProducts?.data || { data: [] }) as { data: AllProductsResponse };
+
+        const product = data.find((p) => p.name === name);
+
+        if (product) {
+          return { data: product };
+        } else {
+          const json = await import("@/mocks/products/one.json");
+          return { data: json.default as GetProductByNameResponse };
+        }
+      },
+    }),
+    getProductById: builder.query<GetProductByIdResponse, GetProductByIdRequest>({
+      // query: (id) => `products/product-id/${id}`,
+      queryFn: async (id, api) => {
+        await simulateLoading();
+
+        const state = api.getState() as RootState;
+        const { data } = (state.api.queries.getAllProducts?.data || { data: [] }) as { data: AllProductsResponse };
+
+        const product = data.find((p) => p.id === id);
+
+        if (product) {
+          return { data: product };
+        } else {
+          const json = await import("@/mocks/products/one.json");
+          return { data: json.default as GetProductByNameResponse };
+        }
+      },
+    }),
+    getProductByUnicoId: builder.query<GetProductByUnicoIdResponse, GetProductByUnicoIdRequest>({
+      // query: (idUnico) => `products/product-id-unico/${idUnico}`,
+      queryFn: async (idUnico, api) => {
+        await simulateLoading();
+
+        const state = api.getState() as RootState;
+        const { data } = (state.api.queries.getAllProducts?.data || { data: [] }) as { data: AllProductsResponse };
+
+        const product = data.find((p) => p.idUnico === idUnico);
+
+        if (product) {
+          return { data: product };
+        } else {
+          const json = await import("@/mocks/products/one.json");
+          return { data: json.default as GetProductByNameResponse };
+        }
+      },
+    }),
+    updateProduct: builder.mutation<UpdateProductResponse, UpdateProductRequest>({
+      // query: (product) => `products/update/${product.id}`,
+      queryFn: async () => {
+        await simulateLoading();
+        const json = await import("@/mocks/products/one.json");
+        return { data: json.default as UpdateProductResponse };
+      },
+    }),
+    createProduct: builder.mutation<CreateProductResponse, CreateProductRequest>({
+      // query:'products/create'
+      queryFn: async () => {
+        await simulateLoading();
+        const json = await import("@/mocks/products/one.json");
+        return { data: json.default as CreateProductResponse };
+      },
+    }),
+    deleteProduct: builder.mutation<DeleteProductResponse, DeleteProductRequest>({
+      queryFn: async (id) => {
+        await simulateLoading();
+        return { data: id };
+      },
+    }),
   }),
 });
 
-export const { useLoginMutation, useForgotPasswordMutation, useRegisterMutation, useGetAllProductsQuery } = api;
+export const {
+  useLoginMutation,
+  useForgotPasswordMutation,
+  useRegisterMutation,
+  useGetAllProductsQuery,
+  useGetProductByIdQuery,
+  useGetProductByNameQuery,
+  useGetProductByUnicoIdQuery,
+  useCreateProductMutation,
+  useDeleteProductMutation,
+  useUpdateProductMutation,
+} = api;
