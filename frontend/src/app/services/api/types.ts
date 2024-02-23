f9252709a7da97a8853ee83c57596b99da711b23
Santiago Ariel <santiagoarielv98@gmail.com>
2024-02-23 11:41:19 -0300

    tipado para productos (api)

--- frontend/src/app/services/api/types.ts
+++ frontend/src/app/services/api/types.ts
@@ -23,3 +23,83 @@ export interface RegisterRequest {
   address: string;
   phone: string;
 }
+
+export type AllProductsResponse = Product[];
+
+export type GetProductByNameRequest = string;
+export type GetProductByNameResponse = Product;
+
+export type GetProductByIdRequest = number;
+export type GetProductByIdResponse = Product;
+
+export type GetProductByUnicoIdRequest = string;
+export type GetProductByUnicoIdResponse = Product;
+
+export type UpdateProductRequest = Product;
+export type UpdateProductResponse = Product;
+
+export type CreateProductRequest = Product;
+export type CreateProductResponse = Product;
+
+export type DeleteProductRequest = number;
+export type DeleteProductResponse = number;
+
+export interface Product {
+  id: number;
+  idUnico: string;
+  name: string;
+  instruction: string;
+  description: string;
+  totalProduction: number;
+  state: boolean;
+  timeEstimatedCompletion: string;
+  processes: Process[];
+  company: Company;
+  active: boolean;
+}
+
+export interface Process {
+  id: number;
+  product: string;
+  subProcesses: SubProcess[];
+  processAttributes: ProcessAttributes;
+}
+
+export interface SubProcess {
+  id: number;
+  process: string;
+  processAttributes: ProcessAttributes;
+}
+
+export interface ProcessAttributes {
+  name: string;
+  timeReal: number;
+  timeAverage: number;
+  timeMargin: number;
+  comment: string;
+  state: boolean;
+  active: boolean;
+  counter: number;
+}
+
+export interface Company {
+  id: number;
+  commonAttribute: CommonAttribute;
+  products: string[];
+  employee: Employee[];
+}
+
+export interface CommonAttribute {
+  email: string;
+  password: string;
+  address: string;
+  phone: string;
+  name: string;
+}
+
+export interface Employee {
+  id: number;
+  role: string;
+  commonAttribute: CommonAttribute;
+  company: string;
+}
