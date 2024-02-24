3056d24bf9f0bbdecf2e163e489a61690877f88f
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2024-02-23 21:00:06 -0300

    feature/PS-82-EmployeesTable

---
+++ frontend/src/pages/Employees/columns.tsx
@@ -0,0 +1,130 @@
+import { ColumnDef, HeaderContext } from "@tanstack/react-table";
+import { Badge } from "@/components/ui/badge";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { BsThreeDotsVertical, BsTrash, BsPencilSquare, BsFileEarmarkText } from "react-icons/bs";
+import { MdOutlinePostAdd } from "react-icons/md";
+import { RxCaretSort } from "react-icons/rx";
+import { Product } from "@/app/services/api/types";
+
+function ColumnSortButton<Tdata>(name: string, { column }: HeaderContext<Tdata, unknown>) {
+  return (
+    <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+      {name}
+      <RxCaretSort className="ml-2 h-4 w-4" />
+    </Button>
+  );
+}
+
+export const columns: ColumnDef<Product>[] = [
+  {
+    id: "name",
+    accessorKey: "name",
+    header: (prop) => ColumnSortButton("Nombre", prop),
+    meta: {
+      headerName: "Nombre",
+    },
+  },
+  {
+    id: "progress",
+    accessorKey: "state",
+    header: (prop) => ColumnSortButton("Estado", prop),
+    sortingFn: (rowA, rowB) => {
+      const { active: activeA } = rowA.original;
+      const { active: activeB } = rowB.original;
+
+      return activeA === activeB ? 0 : activeA ? -1 : 1;
+    },
+    cell: ({ row }) => {
+      const { active } = row.original;
+      const variant = active ? "success" : "destructive";
+      return (
+        <div className="inline-flex items-center">
+          <Badge className="px-1 py-1" variant={variant} />
+          <span className="pl-2">{active ? "Activo" : "Inactivo"}</span>
+        </div>
+      );
+    },
+    meta: {
+      hidden: true,
+    },
+  },
+  {
+    id: "startDate",
+    accessorKey: "startDate",
+    header: (prop) => ColumnSortButton("Fecha Inicio", prop),
+    sortingFn: (rowA, rowB) => {
+      const dateA = new Date(rowA.original.timeEstimatedCompletion);
+      const dateB = new Date(rowB.original.timeEstimatedCompletion);
+      return dateA.getTime() - dateB.getTime();
+    },
+    cell: ({ row }) =>
+      new Date(row.original.timeEstimatedCompletion).toLocaleDateString([], { month: "2-digit", day: "2-digit" }),
+    meta: {
+      hidden: true,
+    },
+  },
+  {
+    id: "jornadaLaboral",
+    accessorKey: "jornadaLaboral",
+    header: "Jornada Laboral",
+    cell: ({ row }) =>
+      new Date(row.original.timeEstimatedCompletion).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }),
+    meta: {
+      hidden: true,
+    },
+  },
+  {
+    id: "cantidadFinalizados",
+    accessorKey: "cantidadFinalizados",
+    header: (prop) => ColumnSortButton("Nombre", prop),
+    meta: {
+      headerName: "Cantidad productos finalizados",
+    },
+  },
+  {
+    id: "actions",
+    meta: {
+      hidden: true,
+    },
+    enableHiding: false,
+    cell: () => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="group h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <BsThreeDotsVertical className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Opciones</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <BsPencilSquare className="mr-2" />
+              Modificar empleado
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <MdOutlinePostAdd className="mr-2" />
+              Agregar nota
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <BsFileEarmarkText className="mr-2" /> Ver detalle
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <BsTrash className="mr-2" /> Eliminar empleado
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
