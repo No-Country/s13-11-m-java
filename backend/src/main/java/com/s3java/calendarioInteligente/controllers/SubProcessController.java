cf82c2e526c7ab2db4f9b4c8487f173b7b2e6620
Lucas <lucasvonelm@gmail.com>
2024-02-24 14:45:21 -0300

    Update SubProcess

--- backend/src/main/java/com/s3java/calendarioInteligente/controllers/SubProcessController.java
+++ backend/src/main/java/com/s3java/calendarioInteligente/controllers/SubProcessController.java
@@ -1,11 +1,11 @@
 package com.s3java.calendarioInteligente.controllers;
 
+import com.s3java.calendarioInteligente.entities.SubProcess;
 import com.s3java.calendarioInteligente.services.impl.SubProcessServiceImpl;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/subprocess")
@@ -17,5 +17,10 @@ public class SubProcessController {
     @GetMapping("/")
     public ResponseEntity<?> getAllSubProcesses(){
         return subProcessService.getAllSubProcesses();
+    }
+
+    @PutMapping("/{subProcessID}")
+    public ResponseEntity<?> updateSubProcessByID(@Valid @RequestBody SubProcess subProcess, @PathVariable Long subProcessID){
+        return subProcessService.updateByID(subProcess ,subProcessID);
     }
 }
