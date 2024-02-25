d0e02273d8319d0fc19788c479789bc95092ef9f
gabriel-remes <gabm.remes@gmail.com>
2024-02-25 20:31:03 -0300

    BACK: FEAT: add client service and controller

---
+++ backend/src/main/java/com/s3java/calendarioInteligente/services/impl/ClientServiceImpl.java
@@ -0,0 +1,20 @@
+package com.s3java.calendarioInteligente.services.impl;
+
+import com.s3java.calendarioInteligente.entities.Client;
+import com.s3java.calendarioInteligente.services.inter.ClientService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class ClientServiceImpl implements ClientService {
+    @Override
+    public Set<Client> getAll() {
+        return null;
+    }
+
+    @Override
+    public Client create() {
+        return null;
+    }
+}
---
+++ backend/src/main/java/com/s3java/calendarioInteligente/services/inter/ClientService.java
@@ -0,0 +1,12 @@
+package com.s3java.calendarioInteligente.services.inter;
+
+import com.s3java.calendarioInteligente.entities.Client;
+
+import java.util.Set;
+
+public interface ClientService {
+
+    public Set<Client> getAll();
+
+    public Client create();
+}
