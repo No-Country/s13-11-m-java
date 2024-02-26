a8bcbd5a5c03d71a8f8be159b093b8c82ad326f9
Santiago Ariel <santiagoarielv98@gmail.com>
2024-02-26 18:08:57 -0300

    cambios

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -28,9 +28,9 @@ function Navbar() {
   return (
     <header className="sticky inset-x-0 top-0 z-50 bg-background py-4 shadow-sm">
       <div className="container flex items-center">
-        <Link to="/" className="flex items-center gap-2">
-          <Logo className="h-14 w-14" fillColor="#00304b" />
-          <span className="mt-4 w-40 select-none font-bold text-[#00304b]">SMART BUSINESS TRACKER</span>
+        <Link to="/" className="flex items-center gap-2 text-primary">
+          <Logo className="h-14 w-14" />
+          <span className="mt-2 w-32 select-none text-sm font-bold">SMART BUSINESS TRACKER</span>
         </Link>
         <div className="flex grow justify-end md:justify-center">
           {isLogin && (
