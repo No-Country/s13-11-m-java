95c46b72ffa02240973b205782ccebd1a25e01fe
Santiago Ariel <santiagoarielv98@gmail.com>
2024-02-26 20:47:27 -0300

    cambios

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -8,14 +8,14 @@ const Sidebar = () => {
   const { pathname } = useLocation();
 
   return (
-    <div className="fixed bottom-4 left-1/2 z-50 h-max -translate-x-1/2 overflow-hidden rounded-lg border bg-background shadow-2xl md:left-4 md:top-1/2 md:-translate-y-1/2 md:translate-x-0">
-      <nav className="max-md:px-6 md:py-6">
+    <div className="fixed bottom-4 left-1/2 z-50 h-max -translate-x-1/2 overflow-hidden rounded-xl border bg-background shadow-2xl md:left-4 md:top-1/2 md:-translate-y-1/2 md:translate-x-0">
+      <nav className="max-md:px-4 md:py-4">
         <ul className="flex max-md:space-x-1 md:flex-col">
           {menuItems.map((item, index) => (
             <li
               key={index}
               data-active={pathname === item.href}
-              className="relative before:absolute before:w-0.5 before:rounded-sm hover:before:bg-primary/50 data-[active=true]:bg-accent before:data-[active=true]:bg-primary max-md:before:bottom-0 max-md:before:h-0.5 max-md:before:w-full md:before:left-0 md:before:h-full"
+              className="relative before:absolute before:w-[3px] before:rounded-sm hover:before:bg-primary/50 data-[active=true]:bg-accent before:data-[active=true]:bg-primary max-md:before:bottom-0 max-md:before:h-0.5 max-md:before:w-full md:before:left-0 md:before:h-full"
             >
               <Button asChild className="h-12 w-12 text-2xl" size="icon" variant="ghost" aria-label={item.label}>
                 <Link to={item.href}>{item.icon}</Link>
