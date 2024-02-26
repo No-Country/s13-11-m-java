f8533e3cec6ba42ccc3798dd84207bfe21fd3398
Santiago Ariel <santiagoarielv98@gmail.com>
2024-02-26 18:11:55 -0300

    agregando logo en authnavbar

--- frontend/src/components/Navbar/AuthNavbar.tsx
+++ frontend/src/components/Navbar/AuthNavbar.tsx
@@ -1,10 +1,15 @@
-import Logo from "../Logo/Logo";
+import { Link } from "react-router-dom";
+
+import Logo from "../Logo";
 
 function AuthNavbar() {
   return (
     <header className="fixed inset-x-0 top-0 z-50 bg-primary py-4 md:bg-transparent">
       <div className="container flex items-center">
-        <Logo className="text-background" />
+        <Link to="/" className="flex items-center gap-2 text-background">
+          <Logo className="h-14 w-14" />
+          <span className="mt-2 w-32 select-none text-sm font-bold">SMART BUSINESS TRACKER</span>
+        </Link>
       </div>
     </header>
   );
