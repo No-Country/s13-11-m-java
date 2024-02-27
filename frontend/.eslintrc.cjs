ec53d55e236b79396ac0ba7ea9762e776bdae3ce
Santiago Ariel <santiagoarielv98@gmail.com>
2024-02-27 09:25:22 -0300

    configuraciones de eslint & prettier

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -13,34 +13,14 @@ module.exports = {
     "plugin:react-hooks/recommended",
     "prettier",
   ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
   settings: {
     react: {
       version: "detect",
     },
-    "import/resolver": {
-      typescript: {},
-    },
   },
-  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "node_modules"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
     "react/react-in-jsx-scope": "off", // Desactiva la regla de react-in-jsx-scope
     "react/prop-types": "off", // Desactiva la regla de prop-types para className
