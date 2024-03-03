e66248713bfcf3188c1e73f3d982c6ab5fbe8440
Santiago Ariel <santiagoarielv98@gmail.com>
2024-03-03 15:20:17 -0300

    nuevo variable para boton & se agrego menubar

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -21,6 +21,7 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         rounded: "h-10 rounded-full px-4 py-2",
         "rounded-xl": "h-10 rounded-full px-16 py-2",
+        xs: "h-7 rounded-md px-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
