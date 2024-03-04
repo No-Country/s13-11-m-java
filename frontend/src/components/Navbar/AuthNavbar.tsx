f1746ff7b8ad318e7363fcb4a18d3fc7fd1835c8
Santiago Ariel <santiagoarielv98@gmail.com>
2024-03-04 09:19:53 -0300

    cambios en los navbars

--- frontend/src/components/Navbar/AuthNavbar.tsx
+++ frontend/src/components/Navbar/AuthNavbar.tsx
@@ -4,7 +4,7 @@ import Logo from "../Logo";
 
 function AuthNavbar() {
   return (
-    <header className="container fixed inset-x-0 top-0 z-50 ml-20 flex items-center bg-primary p-4 md:bg-transparent">
+    <header className="container fixed inset-x-0 top-0 z-50 flex items-center bg-primary p-4 md:bg-transparent">
       <Link to="/" className="flex items-center gap-2">
         <Logo className="h-14 w-14 text-background" />
         <span className="mt-4 w-40 select-none font-bold text-white">SMART BUSINESS TRACKER</span>
